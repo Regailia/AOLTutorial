00cdc4aa2414a96315c60b982a4d8a29d070df04
Binghao <binghao.zhang@mail.utoronto.ca>
2024-03-06 22:05:26 -0500

    Added Ben

--- AOLRoster.py
+++ AOLRoster.py
@@ -30,7 +30,6 @@ class TimLab:
 aol = TimLab()
 aol.add_student("Bing", 30, 1)
 aol.add_student("Craig", 30, 3)
-aol.add_student("Ben", 30, 1)
 
 print(aol.total_cost())
 aol.funding("NSERC", 123456)
